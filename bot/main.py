a46a1940e0abc9ca3ce476aa15a20a79122aa4b9
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2021-08-12 14:25:17 +0500

    bot set

--- bot/main.py
+++ bot/main.py
@@ -1,4 +1,20 @@
+import dotenv
+import os
+import logging
+from telegram.ext import Updater, CommandHandler
+
+dotenv.load_dotenv()
+
+
+def test(update, context):
+    print("Worked!")
 
 
 def main():
-    pass
+    updater = Updater(token=os.getenv("BOT_TOKEN"))
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler('start', test))
+
+    updater.start_polling()
+    updater.idle()
