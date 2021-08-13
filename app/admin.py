a57b190bb9525a53acd15cd27d7368b5d9860f0e
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2021-08-13 14:04:31 +0500

    admin config for user model

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
 from .models import User
 
+admin.site.site_url = None
+admin.site.index_title = "Добро пожаловать!"
+admin.site.site_title = "Администрация «Trader Bot»"
+admin.site.site_header = "Администрация «Trader Bot»"
+
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("id", "first_name", "last_name", "username",
+                    "phone_number", "subscription_status")
+    list_display_links = ("id",)
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
