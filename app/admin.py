9261253c2bb5f153326d1a628459501237e4e130
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2021-08-13 11:34:24 +0500

    delete app

--- app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import User
-
-
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    pass
--- app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AdminappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app'
