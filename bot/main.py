8a555e2361dea723541a94187389f16f6c78f9d8
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2021-08-13 14:08:45 +0500

    main.py updated

--- bot/main.py
+++ bot/main.py
@@ -1,22 +1,41 @@
 import dotenv
 import os
 import logging
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, ConversationHandler
+from bot.src.registration import Registration
 from bot.src.error import error_handler
 
 dotenv.load_dotenv()
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
+)
 
+logger = logging.getLogger(__name__)
 
-def test(update, context):
-    print("Worked!")
+registration = Registration()
 
 
 def main():
     updater = Updater(token=os.getenv("BOT_TOKEN"))
     dispatcher = updater.dispatcher
 
-    dispatcher.add_handler(CommandHandler('start', test))
-    dispatcher.add_error_handler(error_handler)
+    main_conversation = ConversationHandler(
+        entry_points=[
+            CommandHandler('start', registration.start)
+        ],
+        states={
+            "LANGUAGE": [
+
+            ],
+            "MENU_DISPLAYED": [
+
+            ]
+        },
+        fallbacks=[]
+    )
+
+    dispatcher.add_handler(main_conversation)
+    # dispatcher.add_error_handler(error_handler)
 
     updater.start_polling()
     updater.idle()
