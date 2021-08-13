1a32b4477bf99c5bb6aabc8770db9a0c2ea3d782
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2021-08-13 11:28:19 +0500

    initial migration

--- adminapp/models.py
+++ adminapp/models.py
@@ -2,12 +2,14 @@ from django.db import models
 
 
 class User(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.BigIntegerField(primary_key=True)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     username = models.CharField(max_length=255, null=True, blank=True)
     phone_number = models.IntegerField(null=True)
-    joined = models.DateTimeField(null=True)
+    subscription_status = models.BooleanField(null=False, default=False)
+    date_joined = models.DateTimeField(null=True)
+
 
 class Plan(models.Model):
     id = models.AutoField(primary_key=True)
@@ -15,7 +17,9 @@ class Plan(models.Model):
     duration = models.PositiveIntegerField(null=True)
     price = models.PositiveBigIntegerField(null=True, blank=True)
 
+
 class Subscription(models.Model):
     user = models.ForeignKey(User, on_delete=models.PROTECT)
-    plan = models.ForeignKey(Plan, on_delete=models.PROTECT, null=True, blank=True)
+    plan = models.ForeignKey(
+        Plan, on_delete=models.PROTECT, null=True, blank=True)
     paid = models.BooleanField(default=False)
