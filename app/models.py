79e830254bacf5cc665652ccf71130b3ddb4cf6c
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2021-08-13 14:04:17 +0500

    alter user model - change verbose name for all fields

--- app/models.py
+++ app/models.py
@@ -4,13 +4,26 @@ from django.db import models
 
 
 class User(models.Model):
-    id = models.BigIntegerField(primary_key=True)
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    username = models.CharField(max_length=255, null=True, blank=True)
-    phone_number = models.IntegerField(null=True)
-    subscription_status = models.BooleanField(null=False, default=False)
-    date_joined = models.DateTimeField(null=True)
+    class Meta:
+        verbose_name_plural = "Пользователи"
+        verbose_name = "Пользователь"
+
+    id = models.BigIntegerField(primary_key=True, verbose_name="ID")
+    first_name = models.CharField(
+        max_length=255, null=True, verbose_name="Имя", blank=True)
+    last_name = models.CharField(
+        max_length=255, null=True, verbose_name="Фамилия", blank=True)
+    username = models.CharField(
+        max_length=255, null=True, blank=True, verbose_name="Юзернейм")
+    phone_number = models.PositiveBigIntegerField(
+        null=True, verbose_name="Телефон", blank=True)
+    subscription_status = models.BooleanField(
+        null=False, default=False, verbose_name="Статус подписки")
+    date_joined = models.DateField(
+        null=True, verbose_name="Дата присоединения", auto_now_add=True)
+
+    def __str__(self):
+        return str(self.id)
 
 
 class Plan(models.Model):
