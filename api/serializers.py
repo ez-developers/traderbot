cdeb981e68f02d7f85cde262c503c91d81f435cd
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2021-08-14 21:16:44 +0500

    update serializer to fetch only needed data for user model

--- api/serializers.py
+++ api/serializers.py
@@ -5,4 +5,5 @@ from app.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ("id", "first_name", "last_name", "username",
+                  "phone_number", "subscription_status", "language", "date_joined")
