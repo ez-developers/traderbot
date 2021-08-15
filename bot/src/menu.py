2c36e332c3ea4d1e6d029f47b6310ac66dc59307
Javlon <javlon20048@gmail.com>
2021-08-15 16:36:32 +0500

    Remove unnecessary imports

--- bot/src/menu.py
+++ bot/src/menu.py
@@ -7,24 +7,9 @@ from telegram import (ReplyKeyboardMarkup,
                       InlineKeyboardMarkup)
 from telegram.ext import CallbackContext
 from bot.src.text import t, b
-from bot.utils.build_menu import build_menu
 from bot.utils.language import lang
-from bot.utils.request import (parser,
-                               target_category_id,
-                               products_list,
-                               product_det,
-                               notification_on,
-                               get)
-from config.settings import API_URL, API_AUTHENTICATION, BASE_DIR, GROUP_ID
 import logging
-import time
 import json
-import requests
-
-j = json.load(open("bot/assets/text.json", "r"))
-text = j["texts"]
-button = j["buttons"]
-menu_button = j["buttons"]
 
 
 class Menu:
