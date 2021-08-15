2c348647f291085c8e71f281cb72177840ed7f5b
Javlon <javlon20048@gmail.com>
2021-08-15 17:33:23 +0500

    opened profile module

---
+++ bot/src/profile.py
@@ -0,0 +1,13 @@
+from telegram import Update
+from telegram.ext import CallbackContext
+
+
+class Profile():
+    
+    def my_info(self, update: Update, context: CallbackContext):
+        pass
+    
+    
+    def subscription_status(self, update: Update, context: CallbackContext):
+        pass
+    
