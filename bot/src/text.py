78f664d6904bdec2291370549beaedc1cfd44e75
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2021-08-15 15:10:33 +0500

    add docstrings for text and button parser
    - Additional info: t and b methods parse texts from text.json according to given keys
    - Thank you for the commit @nuriddinislamov
    - Myself :)

--- bot/src/text.py
+++ bot/src/text.py
@@ -3,9 +3,27 @@ import json
 j = json.load(open("bot/assets/text.json", "r"))
 
 
-def t(key: str, lang="ru"):
+def t(key: str, lang: str):
+    """b Function that gets the text for the bot from text.json
+
+    Args:
+        key (str): [key for the json to find the right piece of text]
+        lang (str): [language of the given text]
+
+    Returns:
+        [str]: [Text from text.json]
+    """
     return j["texts"][key][lang]
 
 
-def b(key: str, lang="ru"):
+def b(key: str, lang: str):
+    """b Function that gets the text of a button in the bot from text.json
+
+    Args:
+        key (str): [key for the json to find the right piece of text]
+        lang (str): [language of the given text]
+
+    Returns:
+        [str]: [Text from text.json]
+    """
     return j["buttons"][key][lang]
