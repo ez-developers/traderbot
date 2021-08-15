e99f03876251c349dac460cf2904d29def9d174a
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2021-08-15 15:06:12 +0500

    group interaction

---
+++ bot/src/group.py
@@ -0,0 +1,26 @@
+from telegram import Update
+from telegram.ext import CallbackContext
+
+
+class Group:
+
+    def reply_to_user(self, update: Update, context: CallbackContext):
+        try:
+            if update.message.reply_to_message:
+
+                response = update.message.text
+
+                reply_id = update.message.reply_to_message.message_id
+
+                user_id = context.bot_data[reply_id]
+
+                reply = f"<b>MANAGER SAID:</b>\n\n{response}"
+
+                context.bot.send_message(chat_id=user_id,
+                                         text=reply,
+                                         parse_mode='HTML')
+            else:
+                pass
+        except KeyError:
+            update.effective_message.reply_text(
+                "Can't answer to that!")
