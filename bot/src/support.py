1ce08f5229c0e556da314a3eb1c7333452905384
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2021-08-16 14:48:42 +0500

    formatting

--- bot/src/support.py
+++ bot/src/support.py
@@ -9,19 +9,14 @@ from config.settings import GROUP_ID
 
 
 class Support():
+
     def accept(self, update: Update, context: CallbackContext):
         chat_id = update.effective_chat.id
         language = lang(chat_id)
         user_request = update.effective_message
-        user = get(f"users/{chat_id}")
-        user_name = user['name']
-        user_phone = user['phone_number']
         msg = context.bot.forward_message(GROUP_ID,
                                           from_chat_id=chat_id,
                                           message_id=user_request.message_id)
-        context.bot.send_message(GROUP_ID,
-                                 t("user_info".format(user_name, user_phone), language),
-                                 parse_mode='HTML')
         payload = {
             msg.message_id: chat_id
         }
