f8d7e0f204fa31e4e1b640dbf2112b8e111301f8
Javlon <javlon20048@gmail.com>
2021-08-16 11:55:33 +0500

    added funtion to get user's info

--- bot/src/profile.py
+++ bot/src/profile.py
@@ -1,13 +1,19 @@
+from bot.utils.request import get
+from bot.utils.language import lang
 from telegram import Update
 from telegram.ext import CallbackContext
 
 
 class Profile():
-    
+
     def my_info(self, update: Update, context: CallbackContext):
-        pass
-    
-    
+        chat_id = update.effective_chat.id
+        language = lang(chat_id)
+        user = get(f"users/{chat_id}/")
+        
+        context.bot.send_message(chat_id, 
+                                 user)
+
     def subscription_status(self, update: Update, context: CallbackContext):
-        pass
-    
+        chat_id = update.effective_chat.id
+        language = lang(chat_id)
