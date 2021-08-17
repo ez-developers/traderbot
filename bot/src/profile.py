849500828dcfd09b4d429caea03226e5c38d2d46
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2021-08-17 19:14:43 +0500

    New feature! Pay from profile
    - Now users can extend their subscription with payments
    - Available years for extension: 1, 3, 5
    - Backend is fully integrated with the bot

--- bot/src/profile.py
+++ bot/src/profile.py
@@ -1,12 +1,17 @@
 from telegram.ext import CallbackContext
-from telegram import Update, ReplyKeyboardMarkup, KeyboardButton
-from bot.utils.request import get
-from bot.utils.language import lang
+from telegram import Update, ReplyKeyboardMarkup, KeyboardButton, LabeledPrice
+from config.settings import CURRENCY, AMOUNT_TO_PAY, INVOICE_TITLE, INVOICE_DESCRIPTION
+from bot.src.menu import Menu
 from bot.src.text import t, b
-from bot.src.registration import Registration
+from bot.utils.request import get, put
+from bot.utils.language import lang
 import datetime
 import locale
 import logging
+import os
+import dotenv
+
+dotenv.load_dotenv()
 
 
 class Profile():
@@ -41,7 +46,7 @@ class Profile():
             context.bot.send_message(chat_id,
                                      f'{t("subscription_status", language)}: <b>{t("not_active", language)}</b>', parse_mode='HTML')
 
-    def extend_subscription(self, update: Update, context: CallbackContext):
+    def choose_plan(self, update: Update, context: CallbackContext):
         chat_id = update.effective_chat.id
         language = lang(chat_id)
         markup = [
@@ -63,3 +68,90 @@ class Profile():
         logging.info(
             f"{chat_id} - is extending his subscription. Returned state: {state}")
         return state
+
+    def extend_subscription(self, update: Update, context: CallbackContext):
+        chat_id = update.effective_chat.id
+        language = lang(chat_id)
+        years = update.effective_message.text[0]
+        state = "EXTENDING_PAY"
+
+        invoice_payload = "I am extending the subscription"
+        provider_token = os.getenv('PAYMENT_TOKEN')
+        currency = CURRENCY
+        price = AMOUNT_TO_PAY * int(years)
+        prices = [LabeledPrice(b('pay', language), price * 100)]
+
+        context.bot.send_message(chat_id,
+                                 t('pay_the_cheque', language),
+                                 reply_markup=ReplyKeyboardMarkup([
+                                     [KeyboardButton(
+                                         b('cancel_pay', language))]
+                                 ], resize_keyboard=True),
+                                 parse_mode='HTML')
+
+        invoice = context.bot.send_invoice(
+            chat_id,
+            INVOICE_TITLE,
+            INVOICE_DESCRIPTION,
+            invoice_payload,
+            provider_token,
+            currency,
+            prices
+        )
+
+        user_payload = {
+            "invoice_id": invoice.message_id,
+            "years_paid": int(years)
+        }
+        context.user_data.update(user_payload)
+
+        logging.info(
+            f"{chat_id} - extending his subscription. Returning state: {state}")
+        return state
+
+    def precheckout(self, update: Update, context: CallbackContext):
+        query = update.pre_checkout_query
+        if query.invoice_payload != 'I am extending the subscription':
+            query.answer(
+                ok=False, error_message="Что-то пошло не так. Свяжитесь с администраторами...")
+        else:
+            query.answer(ok=True)
+
+    def successful_payment(self, update: Update, context: CallbackContext):
+        chat_id = update.effective_chat.id
+        user = get(f'users/{chat_id}')
+        language = user['language']
+        locale.setlocale(locale.LC_TIME, f'{language}_{language.upper()}')
+        years_paid = context.user_data['years_paid']
+        year = 365
+        subscribed_until = datetime.datetime.strptime(
+            user['subscribed_until'], '%Y-%m-%d')
+        subscribscription_extend = datetime.timedelta(days=year*years_paid)
+        extended_till = subscribed_until + subscribscription_extend
+        user_display_date = datetime.datetime.strftime(
+            extended_till, "%d %B %Y")
+
+        payload = {
+            "id": chat_id,
+            "subscription_status": True,
+            "subscribed_until": f"{extended_till.date()}",
+            "number_of_subscriptions": user['number_of_subscriptions'] + 1
+        }
+
+        put(f"users/{chat_id}/", payload)
+
+        invoice_id = context.user_data['invoice_id']
+        context.bot.delete_message(chat_id,
+                                   message_id=invoice_id)
+        context.bot.send_message(chat_id,
+                                 f"{t('subscription_extended', language)}"
+                                 .format(user_display_date),
+                                 parse_mode='HTML')
+        return Menu().display(update, context)
+
+    def cancel_extending(self, update: Update, context: CallbackContext):
+        chat_id = update.effective_chat.id
+        context.bot.delete_message(chat_id,
+                                   message_id=context.user_data['invoice_id'])
+
+        return self.choose_plan(update, context)
