ae5e6f63f8f0bfcf26b829f1973b7e329d0fa64c
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2021-08-18 15:28:22 +0500

    implement recently added util function
    - all the function does is parses all buttons and gets the id by the name given in ags

--- bot/src/portfolio.py
+++ bot/src/portfolio.py
@@ -1,35 +1,29 @@
 from telegram import Update
 from telegram.ext import CallbackContext
-from bot.utils.request import get, put
+from bot.utils.request import get, put, get_target_id_by_name
 from bot.utils.language import lang
 from bot.src.text import t
-
 
 
 class Portfolio():
 
     def handle_portfolio_click(self, update: Update, context: CallbackContext):
         chat_id = update.effective_chat.id
-        target_portfolio = update.message.text
         language = lang(chat_id)
-        all_portfolios = get('portfolios')
+        id = get_target_id_by_name('portfolios', update.message.text)
 
-        for i in all_portfolios:
-            if i['name'] == target_portfolio:
-                this_portfolio_id = i['id']
+        portfolio = get(f'portfolios/{id}')
 
-        user_choice = get(f'portfolios/{this_portfolio_id}')
-
-        if chat_id in user_choice['user_list']:
+        if chat_id in portfolio['user_list']:
             update.edited_message.reply_text("you have already chosen it")
         else:
-            updated_users = user_choice['user_list'].append(chat_id)
+            updated_users = portfolio['user_list'].append(chat_id)
             payload = {
                 "user_list": updated_users,
                 "user_count": len(updated_users)
             }
 
-            put(f'portfolios/{this_portfolio_id}/', payload)
+            put(f'portfolios/{id}/', payload)
 
             context.bot.send_message(chat_id,
                                      t("thanks", language))
