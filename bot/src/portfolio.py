ed6e3a5524002a31890ac887b444f3442223796b
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2021-08-18 18:29:41 +0500

    COMMITTING BUG

--- bot/src/portfolio.py
+++ bot/src/portfolio.py
@@ -3,6 +3,7 @@ from telegram.ext import CallbackContext
 from bot.utils.request import get, put, get_target_id_by_name
 from bot.utils.language import lang
 from bot.src.text import t
+import json
 
 
 class Portfolio():
@@ -13,17 +14,20 @@ class Portfolio():
         id = get_target_id_by_name('portfolios', update.message.text)
 
         portfolio = get(f'portfolios/{id}')
+        users = []
+        for i in portfolio['user_list']:
+            users.append(i)
+        print(users)
 
-        if chat_id in portfolio['user_list']:
-            update.edited_message.reply_text("you have already chosen it")
-        else:
-            updated_users = portfolio['user_list'].append(chat_id)
-            payload = {
-                "user_list": updated_users,
-                "user_count": len(updated_users)
-            }
+        # TODO: Very huge bug here. Please, help with this, guys!
+        updated_users = ['sdfsd', 'sd'].append(str(chat_id))
+        print(updated_users)
+        payload = {
+            "user_list": updated_users,
+            "user_count": portfolio['user_count'] + 1
+        }
 
-            put(f'portfolios/{id}/', payload)
+        put(f'portfolios/{id}/', payload)
 
-            context.bot.send_message(chat_id,
-                                     t("thanks", language))
+        context.bot.send_message(chat_id,
+                                 t("thanks", language))
