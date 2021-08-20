d58732723d9cdf2c98eb47a42645802be42c87b5
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2021-08-20 15:44:07 +0500

    formatting

--- bot/src/quiz.py
+++ bot/src/quiz.py
@@ -1,14 +1,12 @@
-from bot.utils.language import lang
-from bot.src.text import t, b
 from telegram.ext import CallbackContext
-from telegram import Update, KeyboardButton, ReplyKeyboardMarkup, chat
+from telegram import Update, KeyboardButton, ReplyKeyboardMarkup
+from bot.src.text import t, b
+from bot.utils.language import lang
 import logging
 
 
-
-
 class Quiz():
-    
+
     def question1(self, update: Update, context: CallbackContext):
         chat_id = update.effective_chat.id
         language = lang(chat_id)
@@ -16,13 +14,13 @@ class Quiz():
         back = [KeyboardButton(b("back", language))]
         context.bot.send_message(chat_id,
                                  t("request_capital", language),
-                                 reply_markup = ReplyKeyboardMarkup(
+                                 reply_markup=ReplyKeyboardMarkup(
                                      [back], resize_keyboard=True),
                                  parse_mode='HTML')
         logging.info(
             f"{chat_id} - answering first question. Returned state: {self.question2}")
         return self.question2
-    
+
     def question2(self, update: Update, context):
         chat_id = update.effective_chat.id
         language = lang(chat_id)
@@ -30,32 +28,33 @@ class Quiz():
         back = [KeyboardButton(b("back", language))]
         context.bot.send_message(chat_id,
                                  t("request_expectations", language),
-                                 reply_markup = ReplyKeyboardMarkup(back, resize_keyboard=True),
+                                 reply_markup=ReplyKeyboardMarkup(
+                                     back, resize_keyboard=True),
                                  parse_mode='HTML')
-        
+
         logging.info(
             f"{chat_id} - answering second question. Returned state: {state}")
         return self.question3
-    
+
     def question3(self, update: Update, context: CallbackContext):
         chat_id = update.effective_chat.id
         language = lang(chat_id)
         state = "QUIZ"
         buttons = [
             [KeyboardButton(b("yes", language)),
-            KeyboardButton(b("no", language))]
+             KeyboardButton(b("no", language))]
             [KeyboardButton(b("back", language))]]
-        
+
         context.bot.send_message(chat_id,
                                  t("request_risk", language),
-                                 reply_markup = ReplyKeyboardMarkup(buttons, resize_keyboard=True),
+                                 reply_markup=ReplyKeyboardMarkup(
+                                     buttons, resize_keyboard=True),
                                  parse_mode='HTML')
-        
+
         logging.info(
             f"{chat_id} - answering third question. Returned state: {state}")
         return state
-        
-    
+
     def question4(self, update: Update, context: CallbackContext):
         chat_id = update.effective_chat.id
         language = lang(chat_id)
@@ -63,15 +62,16 @@ class Quiz():
         back = [
             [KeyboardButton(b("back", language))]
         ]
-        
+
         context.bot.send_message(chat_id,
                                  t('request_history', language),
-                                 reply_markup = ReplyKeyboardMarkup(back, resize_keyboard=True),
+                                 reply_markup=ReplyKeyboardMarkup(
+                                     back, resize_keyboard=True),
                                  parse_mode='HTML')
         logging.info(
             f"{chat_id} - answering fourth question. Returned state: {state}")
         return Quiz().accept
-    
+
     def accept(self, update: Update, context: CallbackContext):
         chat_id = update.effective_chat.id
         language = lang(chat_id)
@@ -79,12 +79,13 @@ class Quiz():
         finish = [
             [KeyboardButton(b("finish", language))]
         ]
-        
+
         context.bot.send_message(chat_id,
                                  t('accept_quiz', language),
-                                 reply_markup = ReplyKeyboardMarkup(finish, resize_keyboard=True),
+                                 reply_markup=ReplyKeyboardMarkup(
+                                     finish, resize_keyboard=True),
                                  parse_mode='HTML')
-        
+
         logging.info(
             f"{chat_id} - finishing questionaire. Returned state: {state}")
         return state
