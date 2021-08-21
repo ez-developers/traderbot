65857c78e8e94227aee571362704e0382eaed73d
Jakhongir <jahongirnagmatov@gmail.com>
2021-08-21 13:04:51 +0500

    refactor admin.py: change import settings and format the code

--- app/admin.py
+++ app/admin.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
+from django.http import HttpResponseRedirect
+from core.settings import DJANGO_DEVELOPER_ID, BASE_DIR
 from .models import User, Promo, Portfolio, VideoLesson, Mailings
 from .forms import CustomActionForm
-
-from django.conf import settings
 import time
 import os
 import dotenv
 import telebot
-from django.http import HttpResponseRedirect
 
 dotenv.load_dotenv()
 
@@ -17,6 +16,7 @@ admin.site.site_url = None
 admin.site.index_title = "Добро пожаловать!"
 admin.site.site_title = "Администрация «Trader One™"
 admin.site.site_header = "Администрация Trader One™"
+
 
 @admin.register(Mailings)
 class MailingsAdmin(admin.ModelAdmin):
@@ -36,69 +36,69 @@ class MailingsAdmin(admin.ModelAdmin):
         else:
 
             path = open(
-                str(str(settings.BASE_DIR) + f'/uploads/images/{str(image)}'), "rb")
+                str(str(BASE_DIR) + f'/uploads/images/{str(image)}'), "rb")
 
-            photo = bot.send_photo(os.getenv('DEVELOPER_CHAT_ID'), path, message, parse_mode='HTML')
+            photo = bot.send_photo((DJANGO_DEVELOPER_ID), path, message, parse_mode = 'HTML')
 
-            photo_id = photo.json['photo'][0]['file_id']
+            photo_id=photo.json['photo'][0]['file_id']
 
-            for el in User.objects.values_list('id',).exclude(id=os.getenv('DEVELOPER_CHAT_ID')):
+            for el in User.objects.values_list('id',).exclude(id = DJANGO_DEVELOPER_ID):
                 try:
-                    bot.send_photo(el[0], photo_id, message, parse_mode='HTML')
+                    bot.send_photo(el[0], photo_id, message,
+                                   parse_mode = 'HTML')
                 except Exception as e:
                     print("ID неправылный или бота заблокировал ")
 
         return super(MailingsAdmin, self).response_post_save_add(
             request, obj)
 
-    list_display = ("message", "date_sent", "portfolio",)
-    list_per_page = 50
-    action_form = CustomActionForm
+    list_display=("message", "date_sent", "image", "portfolio",)
+    list_per_page=50
+    action_form=CustomActionForm
 
-@admin.register(User)
+@ admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ("id", "first_name", "last_name", "username",
+    list_display=("id", "first_name", "last_name", "username",
                     "phone_number", "subscription_status", "date_joined", "subscribed_until")
-    exclude = ("number_of_subscriptions", "portfolio")
-    list_display_links = ("id",)
-    list_filter = ("subscribed_until", ('subscription_status',
+    exclude=("number_of_subscriptions", "portfolio")
+    list_display_links=("id",)
+    list_filter=("subscribed_until", ('subscription_status',
                    admin.BooleanFieldListFilter),)
-    search_fields = ("first_name", )
-    list_per_page = 50
+    search_fields=("first_name", )
+    list_per_page=50
 
-    def has_change_permission(self, request, obj=None):
+    def has_change_permission(self, request, obj = None):
         return False
 
     def has_add_permission(self, request):
         return False
 
-    def has_delete_permission(self, request, obj=None):
+    def has_delete_permission(self, request, obj = None):
         return False
 
 
-@admin.register(Promo)
+@ admin.register(Promo)
 class PromoAdmin(admin.ModelAdmin):
-    list_display = ("promo_id", "valid_date", "is_active")
-    readonly_fields = ("promo_id", "is_active")
-    list_per_page = 50
-    action_form = CustomActionForm
+    list_display=("promo_id", "valid_date", "is_active")
+    readonly_fields=("promo_id", "is_active")
+    list_per_page=50
+    action_form=CustomActionForm
 
-    def has_delete_permission(self, request, obj=None):
+    def has_delete_permission(self, request, obj = None):
         return False
 
 
-@admin.register(Portfolio)
+@ admin.register(Portfolio)
 class PortfolioAdmin(admin.ModelAdmin):
-    list_display = ("name",)
-    exclude = ("users_list", "users_count")
-    actions_selection_counter = True
-    action_form = CustomActionForm
-    list_per_page = 50
+    list_display=("name",)
+    exclude=("users_list", "users_count")
+    actions_selection_counter=True
+    action_form=CustomActionForm
+    list_per_page=50
 
 
-@admin.register(VideoLesson)
+@ admin.register(VideoLesson)
 class VideoLessonAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "url")
-    list_per_page = 50
-    action_form = CustomActionForm
-
+    list_display=("id", "name", "url")
+    list_per_page=50
+    action_form=CustomActionForm
