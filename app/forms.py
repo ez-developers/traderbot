80556a506a326216c18806c9b13d87c9aed9bb54
Jakhongir <jahongirnagmatov@gmail.com>
2021-08-23 14:27:40 +0500

    change the label of customactionform

--- app/forms.py
+++ app/forms.py
@@ -1,13 +1,14 @@
 from django import forms
 
+
 class CustomActionForm(forms.Form):
     action = forms.CharField(widget=forms.HiddenInput,
                              initial='delete_selected',
-                             label='Delete Selected'
+                             label=''
                              )
     select_across = forms.BooleanField(
-                                       label='',
-                                       required=False,
-                                       initial=0,
-                                       widget=forms.HiddenInput({'class': 'select-across'}),
-                                       )
+        label='',
+        required=False,
+        initial=0,
+        widget=forms.HiddenInput({'class': 'select-across'}),
+    )
