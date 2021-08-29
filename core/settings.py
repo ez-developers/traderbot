d4e42e99dfa0ee185c1e43f3686e78c37a970645
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2021-08-29 14:53:52 +0500

    update debug config

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = os.environ.get('DEBUG', False) == 'True'
 ADMIN_IDS = [
     361516746,
     15581077,
